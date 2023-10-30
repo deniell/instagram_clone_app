6c7ef8ef45a50d8d877719eac964cab8693f0269
Denis Stahovsky <deniell@i.ua>
2023-10-30 23:59:20 +0200

    - added loginWithGoogle().

--- lib/state/auth/backend/authenticator.dart
+++ lib/state/auth/backend/authenticator.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:instagram_clone_app/state/auth/constants/constants.dart';
 import 'package:instagram_clone_app/state/auth/models/auth_result.dart';
 import 'package:instagram_clone_app/state/posts/typedefs/user_id.dart';
 
@@ -24,14 +25,50 @@ class Authenticator {
       return AuthResult.aborted;
     }
     // exchange token with OAuth credentials
-    final oauthCredentials = FacebookAuthProvider.credential(token);
+    final oAuthCredentials = FacebookAuthProvider.credential(token);
     // try to sign in with oauthCredentials
     try {
-      await FirebaseAuth.instance.signInWithCredential(oauthCredentials);
+      await FirebaseAuth.instance.signInWithCredential(oAuthCredentials);
       return AuthResult.success;
     } on FirebaseAuthException catch (e) {
+      // for Federated authentication use case:
+      // allow user login with same email address using Google or Facebook
+      final email = e.email;
+      final credential = e.credential;
+      if (e.code == Constants.accountExistsWithDifferentCredential &&
+        email != null &&
+        credential != null)
+      {
+        final providers
+          = await FirebaseAuth.instance.fetchSignInMethodsForEmail(email);
+        if (providers.contains(Constants.googleCom)) {
+          await loginWithGoogle();
+          FirebaseAuth.instance.currentUser?.linkWithCredential(credential);
+        }
+        return AuthResult.success;
+      }
+      return AuthResult.failure;
+    }
+  }
 
-
+  Future<AuthResult> loginWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn(scopes: [
+      Constants.emailScope,
+    ]);
+    final signInAccount = await googleSignIn.signIn();
+    if (signInAccount == null) {
+      return AuthResult.aborted;
+    }
+    final googleAuth = await signInAccount.authentication;
+    final oAuthCredentials = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    try {
+      await FirebaseAuth.instance.signInWithCredential(oAuthCredentials);
+      return AuthResult.success;
+    } catch (e) {
+      return AuthResult.failure;
     }
   }
 }
