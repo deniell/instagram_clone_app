b90ee09113f844356c137c0ec71c362975b2a7b3
Denis Stahovsky <deniell@i.ua>
2023-11-06 23:53:00 +0200

    - saveUserInfo() method complated in UserInfoStorage class; - loginWithGoogle() method is complated in AuthStateNotifier class.

--- lib/state/auth/notifiers/auth_state_notifier.dart
+++ lib/state/auth/notifiers/auth_state_notifier.dart
@@ -2,13 +2,15 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone_app/state/auth/backend/authenticator.dart';
 import 'package:instagram_clone_app/state/auth/models/auth_result.dart';
 import 'package:instagram_clone_app/state/auth/models/auth_state.dart';
-
+import 'package:instagram_clone_app/state/posts/typedefs/user_id.dart';
+import 'package:instagram_clone_app/state/user_info/backend/user_info_storage.dart';
 
 ///
 /// Authorization state
 ///
 class AuthStateNotifier extends StateNotifier<AuthState> {
   final _authenticator = const Authenticator();
+  final _userInfoStorage = const UserInfoStorage();
 
   AuthStateNotifier() : super(const AuthState.unknown()) {
     if (_authenticator.isAlreadyLoggedIn) {
@@ -31,5 +33,20 @@ class AuthStateNotifier extends StateNotifier<AuthState> {
     // try to login
     final result = await _authenticator.loginWithGoogle();
     final userId = _authenticator.userId;
+    if (result == AuthResult.success && userId != null) {
+      await saveUserInfo(userId: userId);
+    }
+    state = AuthState(
+      result: result,
+      isLoading: false,
+      userId: userId,
+    );
   }
+
+  Future<void> saveUserInfo({required UserId userId}) =>
+      _userInfoStorage.saveUserInfo(
+        userId: userId,
+        displayName: _authenticator.displayName,
+        email: _authenticator.email,
+      );
 }
--- lib/state/user_info/backend/user_info_storage.dart
+++ lib/state/user_info/backend/user_info_storage.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart' show immutable;
+import 'package:instagram_clone_app/state/constants/firebase_collection_name.dart';
+import 'package:instagram_clone_app/state/constants/firebase_field_name.dart';
 import 'package:instagram_clone_app/state/posts/typedefs/user_id.dart';
+import 'package:instagram_clone_app/state/user_info/models/user_info_payload.dart';
 
 @immutable
 class UserInfoStorage {
@@ -10,8 +14,44 @@ class UserInfoStorage {
     required String displayName,
     required String? email,
   }) async {
+    try {
+      // first check if we have this user's info from before
+      // to avoid instances duplication in Firestore users collection
+      final userInfo = await FirebaseFirestore.instance
+          .collection(
+            FirebaseCollectionName.users,
+          )
+          .where(FirebaseFieldName.userId, isEqualTo: userId)
+          .limit(1)
+          .get();
 
+      if (userInfo.docs.isNotEmpty) {
+        // we already have this user's info, just update it with new values
+        await userInfo.docs.first.reference.update({
+          FirebaseFieldName.displayName: displayName,
+          FirebaseFieldName.email: email ?? '',
+        });
+        return true;
+      }
 
+      // we don't have this user's info from before, create a new user
+      final payload = UserInfoPayload(
+        userId: userId,
+        displayName: displayName,
+        email: email,
+      );
+      // create new user in Firestore
+      await FirebaseFirestore.instance
+          .collection(
+            FirebaseCollectionName.users,
+          )
+          .add(
+            payload,
+          );
+
+      return true;
+    } on Exception catch (e) {
+      return false;
+    }
   }
 }
-
