a73c9ad6e25dd84bd534c83d8a121deef9409562
Denis Stahovsky <deniell@i.ua>
2024-01-03 23:24:35 +0200

    - ImageUploadNotifier is comlated; - added imageUploaderProvider, which notify listeners of [ImageUploadNotifier] about [IsLoading] state.

--- lib/state/image_upload/notifiers/image_upload_notifier.dart
+++ lib/state/image_upload/notifiers/image_upload_notifier.dart
@@ -1,16 +1,26 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image/image.dart' as img;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone_app/state/constants/firebase_collection_name.dart';
 import 'package:instagram_clone_app/state/image_upload/constants/constants.dart';
 import 'package:instagram_clone_app/state/image_upload/exceptions/could_not_build_thumbnail_exception.dart';
+import 'package:instagram_clone_app/state/image_upload/extensions/get_collection_name_from_file_type.dart';
+import 'package:instagram_clone_app/state/image_upload/extensions/get_image_data_aspect_ratio.dart';
 import 'package:instagram_clone_app/state/image_upload/models/file_type.dart';
 import 'package:instagram_clone_app/state/image_upload/typedefs/is_loading.dart';
 import 'package:instagram_clone_app/state/post_settings/models/post_settings.dart';
+import 'package:instagram_clone_app/state/posts/models/post_payload.dart';
 import 'package:instagram_clone_app/state/posts/typedefs/user_id.dart';
+import 'package:uuid/uuid.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
 
+///
+/// Notifier, which responsible for uploading Thumbnails.
+///
 class ImageUploadNotifier extends StateNotifier<IsLoading> {
   ImageUploadNotifier() : super(false);
 
@@ -59,6 +69,58 @@ class ImageUploadNotifier extends StateNotifier<IsLoading> {
         break;
     }
 
-    return false;
+    // calculate the aspect ratio
+    final thumbnailAspectRatio = await thumbnailUint8List.getAspectRatio();
+
+    // calculate references
+    final fileName = const Uuid().v4();
+
+    // create references to the thumbnail and the image itself
+    final thumbnailRef = FirebaseStorage.instance
+        .ref()
+        .child(userId)
+        .child(FirebaseCollectionName.thumbnails)
+        .child(fileName);
+
+    final originalFileRef = FirebaseStorage.instance
+        .ref()
+        .child(fileType.collectionName)
+        .child(fileName);
+
+    // transaction
+    try {
+      // upload the thumbnail
+      final thumbnailUploadTask =
+          await thumbnailRef.putData(thumbnailUint8List);
+      final thumbnailStorageId = thumbnailUploadTask.ref.name;
+
+      // upload the original file
+      final originalFileUploadTask = await originalFileRef.putFile(file);
+      final originalFileStorageId = originalFileUploadTask.ref.name;
+
+      // upload the post itself
+      final postPayload = PostPayload(
+        userId: userId,
+        message: message,
+        thumbnailUrl: await thumbnailRef.getDownloadURL(),
+        fileUrl: await originalFileRef.getDownloadURL(),
+        fileType: fileType,
+        fileName: fileName,
+        aspectRatio: thumbnailAspectRatio,
+        thumbnailStorageId: thumbnailStorageId,
+        originalFileStorageId: originalFileStorageId,
+        postSettings: postSettings,
+      );
+
+      await FirebaseFirestore.instance
+          .collection(FirebaseCollectionName.posts)
+          .add(postPayload);
+
+      return true;
+    } catch (_) {
+      return false;
+    } finally {
+      isLoading = false;
+    }
   }
 }
---
+++ lib/state/image_upload/providers/image_uploader_provider.dart
@@ -0,0 +1,11 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone_app/state/image_upload/notifiers/image_upload_notifier.dart';
+import 'package:instagram_clone_app/state/image_upload/typedefs/is_loading.dart';
+
+///
+/// Provider, which notify listeners of [ImageUploadNotifier] about [IsLoading] state
+///
+final imageUploaderProvider =
+    StateNotifierProvider<ImageUploadNotifier, IsLoading>(
+  (ref) => ImageUploadNotifier(),
+);
